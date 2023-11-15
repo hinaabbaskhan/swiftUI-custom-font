9724ade08daac85a6536ee3b7ab3c146c01b7d22
Hina Khan <47082795+Hana-Khan@users.noreply.github.com>
2023-11-15 11:51:37 +0500

    Initial Commit

---
+++ custom-fonts-practice/custom_fonts_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  custom_fonts_practiceApp.swift
+//  custom-fonts-practice
+//
+//  Created by Hina Khan on 15/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct custom_fonts_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
